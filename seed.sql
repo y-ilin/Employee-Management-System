f5a3d4340d9b3d9a307881dfc45b1dd25bbc1f60
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-03 13:43:59 +1000

    organize seed

--- seed.sql
+++ seed.sql
@@ -1,23 +1,13 @@
 -- Insert sample data
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("B", "T", 1, null), ("K", "L", 1, null), ("CF", "L", 2, 1), ("YL", "L", 3, 2), ("CS", "L", 3, 2);
+VALUES ("Jessica", "Pearson", 1, null), ("Harvey", "Specter", 1, 1), ("Louis", "Litt", 2, 1), ("Mike", "Ross", 3, 2), ("Rachael", "Zane", 3, 3);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Boss", 100000, 1), ("Supervisor", 50000, 1), ("Worker", 1000, 2);
+VALUES ("Boss", 100000, 1), ("Manager", 50000, 2), ("Engineer", 1000, 2);
 
 INSERT INTO department (name)
-VALUES ("Business Strategy"), ("Engineering");
+VALUES ("Business & Strategy"), ("Engineering");
 
--- Insert foreign keys to existing tables
-ALTER TABLE role ADD CONSTRAINT FK_department_id FOREIGN KEY (department_id) REFERENCES department(id);
-ALTER TABLE employee ADD CONSTRAINT FK_role_id FOREIGN KEY (role_id) REFERENCES role(id);
-
--- Join all 3 tables
-SELECT a.id, a.first_name, a.last_name, role.title, department.name AS department, role.salary, CONCAT(b.first_name, " ", b.last_name) AS manager
-FROM employee a
-LEFT JOIN role ON role.id = a.role_id
-LEFT JOIN department ON department.id = role.department_id
-LEFT JOIN employee b ON a.manager_id = b.id;
 
 
 
